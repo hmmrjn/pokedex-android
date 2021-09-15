07360f8cb61d8c92431e545e074ae69228a1b591
hamajun <hmmrjn@gmail.com>
2021-09-15 14:02:09 +0900

    [Update] log api response

--- pokedex/app/src/main/java/com/example/pokedex/scene/pokemon_list/PokemonListFragment.kt
+++ pokedex/app/src/main/java/com/example/pokedex/scene/pokemon_list/PokemonListFragment.kt
@@ -2,11 +2,14 @@ package com.example.pokedex.scene.pokemon_list
 
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.lifecycleScope
 import com.example.pokedex.R
+import com.example.pokedex.infra.pokeapi.PokeApi
 
 class PokemonListFragment : Fragment() {
 
@@ -21,6 +24,13 @@ class PokemonListFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.pokemon_list_fragment, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        lifecycleScope.launchWhenStarted {
+            Log.d("api response", PokeApi.retrofitService.getPokemon().body().toString())
+        }
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
