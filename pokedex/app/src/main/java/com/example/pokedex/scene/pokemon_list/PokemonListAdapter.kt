6d56f256da9ba1442a8d35e4c337044474a18cb7
hamajun <hmmrjn@gmail.com>
2021-09-15 14:03:15 +0900

    [Wip][Add] pokemon list item
    ItemPokemonListBinding の定義が足りない

---
+++ pokedex/app/src/main/java/com/example/pokedex/scene/pokemon_list/PokemonListAdapter.kt
@@ -0,0 +1,22 @@
+package com.example.pokedex.scene.pokemon_list
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class PokemonListAdapter: RecyclerView.Adapter<PokemonListAdapter.ViewHolder>() {
+    inner class ViewHolder(private val binding: ItemPokemonListBinding): RecyclerView.ViewHolder(binding) {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+}
